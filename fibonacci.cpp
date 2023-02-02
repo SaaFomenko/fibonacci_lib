44989156913f3a1d54975f886fface781092c1e8
SaaFomenko <s.fomenko@list.ru>
2023-02-02 16:59:11 +0300

    Fix error in fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,7 +5,7 @@ unsigned long long fibonacci(int number)
 {
 	unsigned long long arr[] = {1, 1};
 
-	for (int i = 2; i < number; ++i)
+	for (int i = 3; i <= number; ++i)
 	{
 		arr[i % 2] = arr[0] + arr[1];
 	}
